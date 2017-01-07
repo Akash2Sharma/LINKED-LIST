e7be4792c116f0477da4298c9e1de8de255a91f0
JIHADI <theakash@live.com>
2017-01-07 22:28:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "ui.h"
-#include "ll.h"
-
+#include "ll.h" 
 int main(int argc, char *argv[]) {
   printf ("enter \'x\' to exit\nS :Single Linked List\nD : Doubly Linked List\nC : Circular LinkedList\n ");
   /* See ui.h, ui.c for execute_command and get_command */
